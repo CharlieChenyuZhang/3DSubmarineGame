2be6504d887e406498077c46ea3cdf30401ba541
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2016-10-27 23:14:15 -0400

    Add files via upload

---
+++ SubmarineController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubmarineController : MonoBehaviour {
+
+	public CharacterController cControl;
+	public Transform submarineHolder;
+	public float speed;
+	public float rotationSpeed;
+	public GameObject missile;
+	public Transform fireSpot;
+
+	// Use this for initialization
+	void Start () {
+		cControl = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Time.deltaTime the time between each frame
+		cControl.Move (submarineHolder.forward * Time.deltaTime * speed);
+		submarineHolder.rotation = Quaternion.Slerp (submarineHolder.rotation,Camera.main.transform.rotation, Time.deltaTime * rotationSpeed);
+		// remember to assign missile and firespot!
+		if (GvrViewer.Instance.Triggered) {
+			Instantiate (missile, fireSpot.position, fireSpot.rotation);
+		}
+	}
+}
---
+++ missileControler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class missileControler : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed;
+
+	float destroyIn = 300;
+	public GameObject explosion;
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * Time.deltaTime * speed;
+
+		destroyIn = destroyIn - 1;
+		if (destroyIn < 0) {
+			DoDestroy ();
+		}
+	}
+			
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Destroy") {
+			Destroy (other.gameObject);
+		}
+		DoDestroy ();
+	}
+
+	void DoDestroy(){
+		Instantiate (explosion, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+}
